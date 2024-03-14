fda43c13a8d156007a69a5359c3f5f0c0817bcfd
grapemoli <wambamgara@gmail.com>
2024-03-14 05:38:16 -0400

    chore: production env bugs

--- app/controllers/bible/index.js
+++ app/controllers/bible/index.js
@@ -1,10 +1,15 @@
 import Controller from '@ember/controller';
 import {tracked} from '@glimmer/tracking';
 import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+
 
 export default class BibleController extends Controller {
   @tracked
   textInput = "";
+
+  @service
+  router;
 
   @action
   submit () {
