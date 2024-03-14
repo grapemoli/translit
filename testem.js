f8ca19f8306ca54d51043d2b8ee0c181138d04ec
grapemoli <wambamgara@gmail.com>
2024-03-14 07:49:55 -0400

    chore: remove ember test script

--- testem.js
+++ testem.js
@@ -16,7 +16,7 @@ module.exports = {
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
-        '--remote-debugging-port=0',
+        '--remote-debugging-port=1234',
         '--window-size=1440,900',
       ].filter(Boolean),
     },
